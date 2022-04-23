b379445c9eec7e76fa25ff2d37f5443c9f01ca87
jonasjesusamerico <jjsamerico@gmail.com>
2022-04-23 16:11:03 -0300

    aplicação de linha de comando

---
+++ aplicacao-linha-comando/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"aplicacao-linha-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	aplicacao := app.Gerar()
+	if erro := aplicacao.Run(os.Args); erro != nil {
+		log.Fatal(erro)
+	}
+}
